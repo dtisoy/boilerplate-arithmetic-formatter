dd223edd55ec53e9d73388e2afb51c85b027c4b6
dtisoy <danieltisoy2001@gmail.com>
2023-05-18 20:47:42 -0500

    fix the main logic
    now the main algorithms is easier to read

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -31,49 +31,73 @@ def check_problems_format(problem):
         raise errorInputArith("Error: Numbers cannot be more than four digits.")
 
 
-def main_logic(problems, result):
-    ops = {"+": lambda x, y: x + y, "-": lambda x, y: x - y}
-    splited = list()
-    # split all the strings word by word
-    for word_string in problems:
-        splited.append(word_string.split())
-    # compare the length of the numbers
-    # the number indexes are 0 and 2
-    thirdline = list()  # this is for the dashes
-
-    fourthline = list()  # this is arithmetic operation
-
-    for item in splited:
-        if result:
-            fourthline.append(str(ops[item[1]](int(item[0]), int(item[2]))))
-        if len(item[0]) > len(item[2]):
-            item[2] = item[2].rjust(len(item[0]))
-            item[0] = f"  {item[0]}"
-            thirdline.append("-" * len(item[0]))
-        else:
-            # here I should add 2 spaces for de + character
-            item[0] = item[0].rjust(len(item[2]) + 2)
-            thirdline.append("-" * len(item[0]))
-
-        item[1] = f"{item[1]} {item.pop(2)}"
-
-    firsline = [x[0] for x in splited]
-    secondline = [x[1] for x in splited]
-    if result:
-        for index in range(len(thirdline)):
-            fourthline[index] = fourthline[index].rjust(len(thirdline[index]))
-
+def format_problem_lines(line_list, get_result=False):
+    """gets a list of splitted problems and return a
+    ready to print string, it will return the operation result as well"""
+    # this will be used to join the final strings
     separator = "    "
-    firsline = separator.join(firsline)
-    secondline = separator.join(secondline)
-    thirdline = separator.join(thirdline)
-    fourthline = separator.join(fourthline)
-    if result:
-        return f"{firsline}\n{secondline}\n{thirdline}\n{fourthline}"
-    return f"{firsline}\n{secondline}\n{thirdline}"
+    # define functions for the defined operations
+    ops = {"+": lambda x, y: int(x) + int(y), "-": lambda x, y: int(x) - int(y)}
+
+    lines = {"line1": [], "line2": [], "line3": [], "result": list()}
+    # the string list format will be:
+    # [number 1, operator, number 2]
+    for num1, op, num2 in line_list:
+        if get_result:
+            solved = str(ops[op](num1, num2))
+
+        # this will take the actual defined lenght
+        # in the current iteration
+        num_length = 0
+        if len(num1) >= len(num2):
+            # now both numbers have the same length
+            num2 = num2.rjust(len(num1))
+            # add the operator the second number
+            num2 = f"{op} {num2}"
+            # define the num_length cause it could be
+            # needed to justify the result
+            num_length = len(num2)
+            # justify the number 1 with the modified number 2
+            # and it's currently stored in num_length
+            num1 = num1.rjust(num_length)
+            # add the digits to their corresponding line
+            lines["line1"].append(num1)
+            lines["line2"].append(num2)
+            # create the dashed separator
+            lines["line3"].append("-" * num_length)
+
+        else:
+            # add the operator to number 2
+            num2 = f"{op} {num2}"
+            # define the num_length cause it could be
+            # needed to justify the result
+            num_length = len(num2)
+            # justify number 1 to the modified number 2
+            num1 = num1.rjust(num_length)
+            # add the digits to their corresponding line
+            lines["line1"].append(num1)
+            lines["line2"].append(num2)
+            # create the dashed separator
+            lines["line3"].append("-" * num_length)
+
+        if get_result:
+            # add the result to the dictionary and justify it with
+            # the current number length
+            lines["result"].append(solved.rjust(num_length))
+
+    for k in lines:
+        lines[k] = separator.join(lines[k])
+
+    # define if the result will be printed or not
+    if get_result:
+        return "{}\n{}\n{}\n{}".format(
+            lines["line1"], lines["line2"], lines["line3"], lines["result"]
+        )
+    else:
+        return "{}\n{}\n{}".format(lines["line1"], lines["line2"], lines["line3"])
 
 
-def arithmetic_arranger(problems, result=False):
+def arithmetic_arranger(problems, get_result=False):
     # check errors
     if len(problems) > 5:
         return "Error: Too many problems."
@@ -84,9 +108,15 @@ def arithmetic_arranger(problems, result=False):
         except errorInputArith as error:
             return str(error)
 
-    # main soluction
-    return main_logic(problems, result)
+    # main solution
+
+    # split the problem parameters
+    splitted_probl = [x.split() for x in problems]
+    # get formatted lines
+    lines = format_problem_lines(splitted_probl, get_result)
+
+    return lines
 
 
 if __name__ == "__main__":
-    print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+    print(arithmetic_arranger(["3801 - 2", "123 + 49"]))
